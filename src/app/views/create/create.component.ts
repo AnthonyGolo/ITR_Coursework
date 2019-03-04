3104df24fffff23f51f785b683c8409f07aff271
Anton Breidak <abreydak@bk.ru>
2019-03-04 20:46:00 +0300

    kkk

--- src/app/views/create/create.component.ts
+++ src/app/views/create/create.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {FirebaseService} from '../../firebase.service';
+import * as firebase from 'firebase';
 
 class Guide {
   title: string;
@@ -11,14 +12,26 @@ class Guide {
   comments: Array<String>;
   constructor(title,
               author,
-              contents,
-              comments) {
+              contents) {
     this.title = title;
     this.author = author;
     this.rating = null;
     this.creationDate = Math.round(new Date().getTime()/1000);
     this.contents = contents;
-    this.comments = comments;
+    this.comments = [];
+  }
+}
+
+class Step {
+  title: string;
+  text: string;
+  editingText: boolean;
+  images: string;
+  constructor(title, text, images) {
+    this.title = title;
+    this.text = text;
+    this.editingText = true;
+    this.images = images;
   }
 }
 
@@ -31,14 +44,17 @@ class Guide {
 export class CreateComponent implements OnInit {
 
 
-  selectedFile = null;
-  steps: Array<Object> = [];
-  images;
+  //uploadedImages: Array<Object> = [];
+  title: string;
+  steps: Array<Step> = [];
+  imagesRef;
+  renamingTitle = false;
 
   constructor(private http: HttpClient,
               private fbs: FirebaseService) {
-    this.images = fbs.storage.ref().child('images');
+    this.imagesRef = fbs.storage.ref().child('images');
     this.addStep();
+    this.title = 'Title';
   }
 
   ngOnInit() {
@@ -46,11 +62,7 @@ export class CreateComponent implements OnInit {
 
   addStep() {
     let i = this.steps.length;
-    this.steps.push({
-      title: 'Step ' + (i+1),
-      text: '',
-      images: []
-    });
+    this.steps.push(new Step( 'Step ' + (i+1), '', [] ));
     console.log(this.steps, 'after addition');
   }
 
@@ -59,13 +71,26 @@ export class CreateComponent implements OnInit {
     console.log(this.steps, 'after removal');
   }
 
-  onFileSelected(event, i){
-    console.log(event);
-    this.selectedFile = event.target.files[0];
+  editField(el, status: boolean) {
+
+  }
+
+  onFileSelected(event, i) {
+    // @ts-ignore
+    let title = document.getElementById('title').value;
+    // UPLOAD TO STORAGE AND PUT LINKS TO ARRAY
+    let imageRef = this.imagesRef.child('guide' + title + 'step' + i + '.jpg');
+    imageRef.put(event.target.files[0]).then(() => {
+      this.steps[i].images = imageRef.getDownloadURL();
+      console.log(this.steps);
+    });
   }
 
   onSubmit()  {
-    // this.http.post('')
+    // @ts-ignore
+    let title = document.getElementById('title').value;
+    let author = firebase.auth().currentUser.displayName;
+    let submittedGuide = new Guide (title, author, this.steps);
   }
 
 }
