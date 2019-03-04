3edf2990308e35602f472d1d1b6f273ea75347f8
Anton Breidak <abreydak@bk.ru>
2019-03-04 13:01:56 +0300

    ew

--- src/app/views/create/create.component.ts
+++ src/app/views/create/create.component.ts
@@ -2,6 +2,27 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {FirebaseService} from '../../firebase.service';
 
+class Guide {
+  title: string;
+  author: string;
+  rating: number;
+  creationDate: number;
+  contents: Array<Object>;
+  comments: Array<String>;
+  constructor(title,
+              author,
+              contents,
+              comments) {
+    this.title = title;
+    this.author = author;
+    this.rating = null;
+    this.creationDate = Math.round(new Date().getTime()/1000);
+    this.contents = contents;
+    this.comments = comments;
+  }
+}
+
+
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
@@ -11,18 +32,34 @@ export class CreateComponent implements OnInit {
 
 
   selectedFile = null;
-  steps = [];
+  steps: Array<Object> = [];
   images;
 
   constructor(private http: HttpClient,
               private fbs: FirebaseService) {
     this.images = fbs.storage.ref().child('images');
+    this.addStep();
   }
 
   ngOnInit() {
   }
 
-  onFileSelected(event){
+  addStep() {
+    let i = this.steps.length;
+    this.steps.push({
+      title: 'Step ' + (i+1),
+      text: '',
+      images: []
+    });
+    console.log(this.steps, 'after addition');
+  }
+
+  removeStep(){
+    this.steps.pop();
+    console.log(this.steps, 'after removal');
+  }
+
+  onFileSelected(event, i){
     console.log(event);
     this.selectedFile = event.target.files[0];
   }
