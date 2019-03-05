f3573319bb06c68ea1b5ddca7e8aace2051d93ae
Anton Breidak <abreydak@bk.ru>
2019-03-05 18:35:45 +0300

    prrrrrrrrrrrrrrrrrrrrr

--- src/app/views/create/create.component.ts
+++ src/app/views/create/create.component.ts
@@ -25,7 +25,7 @@ class Guide {
 class Step {
   title: string;
   text: string;
-  images: string;
+  images: Array<object>;
   constructor(title, text, images) {
     this.title = title;
     this.text = text;
@@ -43,6 +43,7 @@ export class CreateComponent implements OnInit {
 
 
   title: string;
+  author: string;
   steps: Array<Step> = [];
   imagesRef;
 
@@ -50,7 +51,8 @@ export class CreateComponent implements OnInit {
               private fbs: FirebaseService) {
     this.imagesRef = fbs.storage.ref().child('images');
     this.addStep();
-    this.title = 'Title';
+    this.title = '';
+    this.author = firebase.auth().currentUser.displayName;
   }
 
   ngOnInit() {
@@ -58,7 +60,7 @@ export class CreateComponent implements OnInit {
 
   addStep() {
     let i = this.steps.length;
-    this.steps.push(new Step( 'Step ' + (i+1), '', [] ));
+    this.steps.push(new Step( 'Step ' + (i+1), '', [undefined] ));
     console.log(this.steps, 'after addition');
   }
 
@@ -70,33 +72,37 @@ export class CreateComponent implements OnInit {
   editField(event) {
     let text = event.srcElement.innerText;
     let id = event.srcElement.id;
-    if (id.includes('text')) {
+    if (id == 'headline') this.title = text;
+    else if (id.includes('text')) {
       let i = id.replace('text', '');
-
+      this.steps[i].text = text;
     }
-    else if (id.includes('title')) {
+    else {
       let i = id.replace('title', '');
+      this.steps[i].title = text;
     }
   }
 
-  onFileSelected(event, i) {
+  onFileSelected(event, i, fileIndex) {
     // @ts-ignore
-    let title = document.getElementById('title').value;
-    // UPLOAD TO STORAGE AND PUT LINKS TO ARRAY
-    let imageRef = this.imagesRef.child('guide' + title + 'step' + i + '.jpg');
-    imageRef.put(event.target.files[0]).then(() => {
-      this.steps[i].images = imageRef.getDownloadURL();
-      console.log(this.steps);
+    let title = document.getElementById('headline').innerText.replace(' ', '');
+    let n = this.steps[i].images.length;
+    let image = event.target.files[0];
+    let imageRef = this.imagesRef.child(title + 'step' + i + 'pic' + n + '.jpg');
+    imageRef.put(image).then(() => {
+      imageRef.getDownloadURL().then(link => {
+        this.steps[i].images[fileIndex+1] = link;
+        console.log(this.steps[i].images);
+      });
     });
   }
 
   onSubmit()  {
     // @ts-ignore
-    let title = document.getElementById('title').value;
-    let author = firebase.auth().currentUser.displayName;
-    let submittedGuide = new Guide (title, author, this.steps);
+    let submittedGuide = new Guide (this.title, this.author, this.steps);
+    console.log(submittedGuide);
   }
 
-  // TODO inline editing, db upload guide, list of guides: browse, new, best
+  // TODO guide upload to db (mapping), list of guides: browse, new, best
 
 }
